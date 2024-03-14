534a4ca3e500339c594f75f18e9470c6cdf857b1
dmitrii-mv4 <dmitrii_mv4@mail.ru>
2024-03-14 19:47:12 +0300

    Добавление символической ссылки для папки public_html

--- config/filesystems.php
+++ config/filesystems.php
@@ -80,6 +80,8 @@ return [
     'links' => [
         // public_path('storage') => storage_path('app/public'),
         public_path('content/articles') => storage_path('app/public/content/articles'),
+
+        base_path('public_html') => base_path('public'),
     ],
 
 ];
